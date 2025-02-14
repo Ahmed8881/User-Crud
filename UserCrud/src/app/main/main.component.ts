ac68e920a635eb94d0f26d0cf5c3e2aa23f60cfd
Ahmed8881 <buttahmed2004@gmail.com>
2025-02-14 21:05:41 +0500

    add User interface to main component for user data structure

--- UserCrud/src/app/main/main.component.ts
+++ UserCrud/src/app/main/main.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+}
 
 @Component({
   selector: 'app-main',
